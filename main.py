f56bcd63509db13b9a54faffd1dfe67a15c5d409
Rohit Mudgil <rohit.mudgil@airtel.com>
2024-03-16 19:54:53 +0530

    created mvp with basic requirements

---
+++ main.py
@@ -0,0 +1,46 @@
+# Using flask to make an api 
+# import necessary libraries and functions 
+from flask import Flask, jsonify, request, render_template, redirect
+
+from IdeaGenerator import getResponseFromGemini
+
+# creating a Flask app 
+app = Flask(__name__) 
+
+# on the terminal type: curl http://127.0.0.1:5000/ 
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/home', methods = ['GET', 'POST']) 
+def home(): 
+	if(request.method == 'POST'): 
+		return redirect("/result",code=307)
+	# return render_template("index.html") 
+	return render_template("index.html")
+
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+	domain = request.form.get("domain")
+	tech = request.form.get("tech")
+	days = request.form.get("days")
+	promptValues = [domain, tech, days]
+	getDefaultPromptValues(promptValues)
+	return getResponseFromGemini(promptValues)
+
+# A simple function to calculate the square of a number 
+# the number to be squared is sent in the URL when we use GET 
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10 
+# this returns 100 (square of 10) 
+@app.route('/home/<int:num>', methods = ['GET']) 
+def disp(num): 
+
+	return jsonify({'data': num**2}) 
+
+def getDefaultPromptValues(promptValues):
+	for i in range(len(promptValues)):
+		if(promptValues[i]== ''):
+			promptValues[i] = 'any'
+
+# driver function 
+if __name__ == '__main__': 
+
+	app.run(debug = True) 
